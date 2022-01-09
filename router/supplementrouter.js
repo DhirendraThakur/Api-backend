a117e1a2452fa2ad00327122c70d9be17dcdad9b
Dhirendra Thakur <drthunk@gmail.com>
2022-01-09 22:14:22 +0545

    Update code has been done with auth validation

--- router/supplementrouter.js
+++ router/supplementrouter.js
@@ -1,6 +1,8 @@
 //const { Router } = require("express");
 const express = require ("express");
 const Supplement = require("../models/supplementmodel");
+const auth = require("../auth/auth");
+
 const router = new express.Router();
 
 router.post("/supplement/add",function(req,res){
@@ -23,4 +25,32 @@ router.post("/supplement/add",function(req,res){
     })
 
 })
+// to update supplement
+router.put('/supplement/update', auth.verifyCustomer, function(req,res){
+    const sid = req.body.sid;
+    const supplementname = req.body.supplementname;
+    const supplementtype = req.body.supplementtype;
+    const supplementrate = req.body.supplementrate;
+   Supplement.updateOne({_id : sid},{supplementname:supplementname, supplementtype: supplementtype, supplementrate: supplementrate})
+    .then(function(){
+        res.json({message:'Supplement has been updated'})
+    })
+    .catch(function(){
+        res.json({message: 'Somethings went Wrong'})
+    })
+
+})
+
+//to delete supplement
+router.delete('/supplement/delete', auth.verifyCustomer, function(req,res){
+    const sid = req.body.sid;
+Supplement.deleteOne({_id : sid})
+.then (function(){
+    res.json({message:"You have just delete supplement"})
+})
+.catch(function(){
+    res.json({message: 'Somethings went wrong'})
+})
+})
+
 module.exports = router
