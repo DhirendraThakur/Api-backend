39a54c897b48040156e853011623e939ec98dba0
Dhirendra Thakur <drthunk@gmail.com>
2022-01-21 10:55:55 +0545

    Workout router has been modified

--- router/workrouter.js
+++ router/workrouter.js
@@ -1,17 +1,17 @@
-const express = require ("express");
+const express = require("express");
 const Work = require("../models/workmodel");
-const jwt = require ("jsonwebtoken");
+//const jwt = require ("jsonwebtoken");
 
 const router = new express.Router();
 
 router.post("/workout/add", function(req,res){
 
-    console.log(req.body)
+   // console.log(req.body)
     const workname= req.body.workname;
     const worktype = req.body.worktype;
     const data = new Work({
-        workname:workname,
-        worktype:worktype
+        workname : workname,
+        worktype : worktype
     }) 
     data.save()
     .then(function(){
@@ -21,4 +21,4 @@ router.post("/workout/add", function(req,res){
         res.json(e)
     })
 })
-module.exports = router;
+module.exports= router;
